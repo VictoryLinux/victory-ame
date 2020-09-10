88e318373c90004d30a8530ecb8e2abd61e0e5a9
VictoryLinux <63359543+VictoryLinux@users.noreply.github.com>
2020-09-10 12:58:19 -0500

    Update Victory-Win-AME.ps1

--- AME_Iso/Victory-Win-AME.ps1
+++ AME_Iso/Victory-Win-AME.ps1
@@ -234,37 +234,9 @@ Function SetLockscreen {
 }
 
 Function SetWallpaperSlideshow {
-	Add-Type @"
-	using System;
-	using System.Runtime.InteropServices;
-	using Microsoft.Win32;
-	namespace Wallpaper {
-
-	   public class Setter {
-	      public const int SetDesktopWallpaper = 20;
-	      public const int UpdateIniFile = 0x01;
-	      public const int SendWinIniChange = 0x02;
-
-	      [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
-	      private static extern int SystemParametersInfo (int uAction, int uParam, string lpvPara, int fuWinIni);
-
-	      public static void SetWallpaper (string path) {
-
-	         SystemParametersInfo(SetDesktopWallpaper, 0, path, UpdateIniFile | SendWinIniChange);
-
-	         RegistryKey key = Registry.CurrentUser.OpenSubKey("Control Panel\\Desktop", true);
-
-	         //"Fit" style
-	         key.SetValue(@"WallpaperStyle", "6");
-	         key.SetValue(@"TileWallpaper", "0");
-
-	         key.Close();
-	      }
-	   }
-	}
-	"@
-
-	[Wallpaper.Setter]::SetWallpaper("C:\Users\user\Pictures\Backgrounds\victory1.jpg")
+	Write-Output "Setting Up Wallpaper Slideshow... " -NoNewline
+	copy-item 'C:\victory-win\AME_Iso\slideshow.ini' -Destination 'C:\Users\user\AppData\Roaming\Microsoft\Windows\Themes'
+	Write-Host "Complete" -ForegroundColor Green
 }
 
 Function RenameUser {
