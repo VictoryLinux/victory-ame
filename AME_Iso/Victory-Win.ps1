c1f1754cac8306c2152bdcabe87296ab53568d89
VictoryLinux <63359543+VictoryLinux@users.noreply.github.com>
2020-09-10 09:54:41 -0500

    Update Victory-Win.ps1

--- AME_Iso/Victory-Win.ps1
+++ AME_Iso/Victory-Win.ps1
@@ -18,8 +18,10 @@ $tweaks = @(
     "RequireAdmin",
 #    "wifi",
     "Restorepoint",
+    "Uninstall",
     "UpdateInstalledApps",
 #    "InstallVictoryProgs",
+    "RestartExplorer",
     "InstallAdobe",
     "InstallJava",
     "Install7Zip",
@@ -36,7 +38,8 @@ $tweaks = @(
     "Backgrounds",
     "RenameUser",
     "ChangeUserPasswd",
-    "ChangeAdminPasswd"
+    "ChangeAdminPasswd",
+    "Restart"
 #    "Wait",
 #    "LaunchSU",
 #    "su",
@@ -58,83 +61,114 @@ Function Restorepoint {
 }
 
 Function Uninstall {
-	Write-Output "Uninstalling Thunderbird"
+	Write-Output "Uninstalling Thunderbird... " -NoNewline
 	choco uninstall thunderbird -y
+	Write-Host "Complete" -ForegroundColor Green
 }
 	
 Function UpdateInstalledApps {
-	Write-Output "Updateing Installed Packages"
+	Write-Output "Updateing Installed Packages... " -NoNewline
 	choco upgrade all -y
+	Write-Host "Complete" -ForegroundColor Green
+}
+
+Function RestartExplorer {
+	Write-Output "Restarting Explorer.exe... " -NoNewline
+	Start-Process "C:\LenovoSetup\setup\explorer.bat" -Verb runas
+	Start-Sleep -s 15
+	Write-Host "Complete" -ForegroundColor Green
 }
 
 Function InstallVictoryProgs {
-	Write-Output "Installing Chocolatey"
+	Write-Output "Installing Chocolatey... " -NoNewline
 	Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 	choco install chocolatey-core.extension -y
+	Write-Host "Complete" -ForegroundColor Green
 }
 
 Function InstallAdobe {
-	Write-Output "Installing Adobe Acrobat Reader"
+	Write-Output "Installing Adobe Acrobat Reader... " -NoNewline
 	choco install adobereader -y
+	Write-Host "Complete" -ForegroundColor Green
 }
 
 Function InstallJava {
-	Write-Output "Installing Java"
+	Write-Output "Installing Java... " -NoNewline
 	choco install jre8 -y
+	Write-Host "Complete" -ForegroundColor Green
 }
 
 Function Install7Zip {
-	Write-Output "Installing 7-Zip"
+	Write-Output "Installing 7-Zip... " -NoNewline
 	choco install 7zip -y
+	Write-Host "Complete" -ForegroundColor Green
 }
 
 Function InstallBrave {
-	Write-Output "Installing Brave Browser"
+	Write-Output "Installing Brave Browser... " -NoNewline
 	choco install brave -y
 	Copy-Item 'C:\Users\Administrator\Desktop\Brave.lnk' -Destination 'C:\Users\user\Desktop\Brave.lnk'
+	Write-Host "Complete" -ForegroundColor Green
 }
 
 Function InstallChromium {
-	Write-Output "Installing Chromium Browser"
+	Write-Output "Installing Chromium Browser... " -NoNewline
 	choco install chromium -y
+	Write-Host "Complete" -ForegroundColor Green
 }
 
 Function InstallFirefox {
-	Write-Output "Installing Firefox Browser"
-    choco install firefox -y
+	Write-Output "Installing Firefox Browser... " -NoNewline
+	choco install firefox -y
+	Write-Host "Complete" -ForegroundColor Green
 }
 
 Function InstallSublimeText {
-	Write-Output "Installing Sublime Text 3"
-	choco install notepadplusplus -y
+	Write-Output "Installing Sublime Text 3... " -NoNewline
+	choco install sublimetext3 -y
+	Write-Host "Complete" -ForegroundColor Green
 }
 
 Function InstallVLC {
-	Write-Output "Installing VLC Media Player"
+	Write-Output "Installing VLC Media Player... " -NoNewline
 	choco install vlc -y
+	Write-Host "Complete" -ForegroundColor Green
 }
 
 Function Backgrounds {
-	Write-Output "Setting Up Desktop Wallpaper"
+	Write-Output "Setting Up Desktop Wallpaper... " -NoNewline
 	Copy-Item 'C:\victory-win\Backgrounds' -Destination 'C:\Users\user\Pictures'
+	Write-Host "Complete" -ForegroundColor Green
 }
 
 Function RenameUser {
+	Write-Output "Renaming User Account... " -NoNewline
 	Rename-LocalUser -Name "user" -NewName "Nimda"
+	Write-Host "Complete" -ForegroundColor Green
 }
 
 Function ChangeUserPasswd {
+	Write-Output "Changing Local User Account Password... " -NoNewline
 	Get-LocalUser
 	$Password = Read-Host "Enter the new password" -AsSecureString
 	$UserAccount = Get-LocalUser -Name "nimda"
 	$UserAccount | Set-LocalUser -Password $Password
+	Write-Host "Complete" -ForegroundColor Green
 }
 
 Function ChangeAdminPasswd {
+	Write-Output "Changing Local Administrator Account Password... " -NoNewline
 	Get-LocalUser
 	$Password = Read-Host "Enter the new password" -AsSecureString
 	$UserAccount = Get-LocalUser -Name "Administrator"
 	$UserAccount | Set-LocalUser -Password $Password
+	Write-Host "Complete" -ForegroundColor Green
+}
+
+Function Restart {
+	Write-Output "Restarting Windows, to log in as New User... " -NoNewline
+	Write-Host "Complete" -ForegroundColor Green
+	Restart-Computer
 }
 
 # Normalize path to preset file
