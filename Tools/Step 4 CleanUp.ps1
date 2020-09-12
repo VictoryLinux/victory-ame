ebd9e902fd84c023551ce1f9a75f81b27076ba02
VictoryLinux <63359543+VictoryLinux@users.noreply.github.com>
2020-09-12 11:22:58 -0500

    Add files via upload

---
+++ Tools/Step 4 CleanUp.ps1
@@ -0,0 +1,10 @@
+# Clean Up
+
+If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator")) {
+        Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`" $PSCommandArgs" -WorkingDirectory $pwd -Verb RunAs
+        Exit
+    }
+
+Write-Output "Step 3 - Cleaning up before Amelioration"
+
+dism /online /Cleanup-Image /StartComponentCleanup
